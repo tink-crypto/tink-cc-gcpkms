8a0bea871d97653ecd3c78c2d0692064fe206a7e
Moreno Ambrosin <ambrosin@google.com>
2022-12-05 06:00:59 -0800

    Remove the use of `use_bazel.sh` on GCP Ubuntu tests
    PiperOrigin-RevId: 492984131
    Change-Id: Ie2cb74a3e863cfd583f48f1d44771fc3cc314ded

--- kokoro/gcp_ubuntu/bazel/run_tests.sh
+++ kokoro/gcp_ubuntu/bazel/run_tests.sh
@@ -20,8 +20,6 @@ set -euo pipefail
 if [[ -n "${KOKORO_ARTIFACTS_DIR:-}" ]]; then
   TINK_BASE_DIR="$(echo "${KOKORO_ARTIFACTS_DIR}"/git*)"
   cd "${TINK_BASE_DIR}/tink_cc_gcpkms"
-  chmod +x "${KOKORO_GFILE_DIR}/use_bazel.sh"
-  "${KOKORO_GFILE_DIR}/use_bazel.sh" "$(cat .bazelversion)"
 fi
 
 : "${TINK_BASE_DIR:=$(cd .. && pwd)}"
